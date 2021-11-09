0ecbe6c472f6dce89fa2a64c6fa95d3e5299e28a
kimhyerin0909 <khyerin0909@gmail.com>
2021-11-09 22:19:16 +0900

    session이 먼데 씹더가!

--- php/loginProcess.php
+++ php/loginProcess.php
@@ -1,3 +1,6 @@
+<?php 
+  include '../php/inc_head.php';
+?>
 <?php
 $cas_conn = mysqli_connect("localhost", "root", "1234", "user_info", "3306");
 $useremail = $_POST['useremail'];
@@ -7,25 +10,24 @@ $sql = "SELECT * FROM users WHERE useremail ='{$useremail}'";
 $result = mysqli_query($cas_conn, $sql);
 $member = $result->fetch_array();
 $db_pwd = $member['password'];
-//$row['useremail'];
+$row['useremail'];
 
 //$passwordResult = password_verify($password, $hashedPassword);
-if ($password === $db_pwd) {
-    // 로그인 성공
+if ($password === $db_pwd) { // 로그인 성공
     // 세션에 useremail 저장
     session_start();
-    $_SESSION['userEmail'] = $row['useremail'];
-    print_r($_SESSION);
-    echo $_SESSION['userEmail'];
-    echo '<script>
-        alert("로그인에 성공하였습니다.")
-        location.href = "../html/loginState.html";
-    </script>';
+    $_SESSION['useremail'] = $row['useremail'];
+    print_r($_SESSION);  //Array ( [userEmail] => )
+    echo ($_SESSION['useremail']);
+    // echo '<script>
+    //     alert("로그인에 성공하였습니다.")
+    //     location.href = "../html/loginState.html";
+    // </script>';
 } else {
     // 로그인 실패 
     echo '
     <script>
-        alert("로그인에 실패하였습니다");
+        alert("이메일 또는 비밀번호가 잘못되었습니다.");
         location.href = "../html/login.html";
     </script>
     ';
--- php/logout.php
+++ php/logout.php
@@ -1,2 +1,7 @@
+<?php 
+  include '../php/inc_head.php';
+?>
 <?php
+session_start();
+session_destroy();
 ?>
