47cc5ee3b87071a02ecd8d575cdb1dd4c2a79df4
kimhyerin0909 <khyerin0909@gmail.com>
2021-11-17 09:41:11 +0900

    엥 작성하기 안됨!!

--- php/write_action.php
+++ php/write_action.php
@@ -4,14 +4,14 @@ $title = $_POST['title'];
 $language = $_POST['language'];
 $content = $_POST['content'];
 $date = date('Y-m-d H:i:s');
-$URL = '../html/qnaLogin.html';
+$URL = '../php/qnaLogin.php';
 
 $cas_sql_add_user = "INSERT INTO board.board (number, hit, title, content, language, date) VALUES (null, 0, '$title', '$content', '$language', '$date')";
 $result = $db->query($cas_sql_add_user);
 if($result) {
     echo '<script>alert("글이 작성됐습니다."); location.href="../php/qnaLogin.php";</script>';
 } else {
-    echo '<script>alert("실패임 ㅅㄱ링~!");</script>';
+    echo '<script>alert("실패임 ㅅㄱ링~!"); location.href="../php/qnaLogin.php";</script>';
 }
 
 mysqli_close($db);
